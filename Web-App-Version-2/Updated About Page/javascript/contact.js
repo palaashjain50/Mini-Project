c12b9df7834f252b22435eb1ad844c3613cc3399
palaashjain50 <124269943+palaashjain50@users.noreply.github.com>
2023-03-25 20:09:01 +0530

    V2

--- Web-App-Version-2/Updated About Page/javascript/contact.js
+++ Web-App-Version-2/Updated About Page/javascript/contact.js
@@ -4,6 +4,8 @@ var typed = new Typed('#slogan', {
     typeSpeed: 20,
 });
 
+var reviews = ["I recently switched to a new medication for my allergies and it has been a game-changer. I was constantly struggling with congestion and sneezing, but now my symptoms are completely under control. The only downside is that it can make me a bit drowsy, but I've found that taking it before bed helps me get a good night's sleep.", "I've been taking this medication for my chronic pain for a few weeks now, and I have to say that it's been a disappointment. It hasn't really helped alleviate my symptoms and I've experienced some unpleasant side effects like nausea and dizziness. I'm going to speak to my doctor about trying a different medication.", "I suffer from anxiety and panic attacks and was prescribed this medication by my doctor. I was hesitant to take it at first, but it has been a lifesaver. It helps me stay calm and focused during the day and I haven't experienced any negative side effects. I highly recommend it to anyone struggling with anxiety."];
+
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
@@ -12,6 +14,7 @@ var submit = document.getElementById("submit-button");
 var form_content = document.getElementById("form_content");
 var submit_animation = document.getElementById("submit_animation");
 var submission = document.getElementById("submission");
+var form = document.getElementById("form");
 
 submit.addEventListener("click", function(){
     var name_input = ((document.getElementById("name")).value).length;
@@ -22,7 +25,8 @@ submit.addEventListener("click", function(){
     else
     {
         form_content.style.display = "none";
+        form.style.background = "none";
         submit_animation.style.display = "flex";
-        delay(1500).then(() => {submit_animation.style.display = "none"; submission.style.display = "flex"});
+        delay(1500).then(() => {submit_animation.style.display = "none"; submission.style.display = "block"});
     }
 });
