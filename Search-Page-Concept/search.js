7c5f916d02779942397db40322ee3a0e717ccafb
Shanouf22 <mohammed.ansari@spit.ac.in>
2023-04-02 18:29:13 +0530

    search functionality updated

--- Search-Page-Concept/search.js
+++ Search-Page-Concept/search.js
@@ -12,6 +12,8 @@ var inputVal=document.getElementById("medicine_name");
 let sources = ['Tata1mg', 'Apollo Pharmacy', 'Pharmeasy'];
 var miniCards = [];
 var count = 0;
+var addToCartBtns, addIcons, subtractIcons, cart_view, cartIcon, qtyInCart, medNameInCart;
+
 //selecting some elements to make changes in their relative structure when summary is displayed
 let resultBox = document.getElementById("searchResultBox");
 let summaryBox = document.getElementById("summary_container");
@@ -19,6 +21,146 @@ let summaryBtn = document.querySelector("#summaryBtn");
 summaryBtn.classList.add("summaryBtnInactive");
 let notificationMessContainer = document.querySelector(".notificationMessContainer");
 notificationMessContainer.classList.add("notifInactive");
+
+//function for shaking cart btn when already added
+function shakeCart(event){
+    event.target.style.animation='shake 0.5s';
+    event.target.style.backgroundColor='#be0404';
+    event.target.innerText='Already Added!'
+    setTimeout(()=>{
+        event.target.style.animation='';
+    },500);
+}
+
+function triggerClick(event){
+    event.target.parentElement.parentElement.parentElement.childNodes[5].childNodes[1].click();
+}
+
+//functions for add and subtract icons of the main cards
+function mainAddIconEvent(event){
+    var displayElem = event.target.parentElement.parentElement.childNodes[3];
+    var currentVal = parseInt(displayElem.innerText);
+    displayElem.innerText=currentVal+1;
+}
+
+function mainSubIconEvent(event){
+    var displayElem = event.target.parentElement.parentElement.childNodes[3];
+    var currentVal = parseInt(displayElem.innerText);
+    if(currentVal>=2){
+        displayElem.innerText=currentVal-1;
+    }
+}
+
+//function for when something is added to the card
+function addingToCart(event){
+    count++;
+    event.target.parentElement.parentElement.childNodes[3].childNodes[1].childNodes[1].removeEventListener('click',mainAddIconEvent);
+    event.target.parentElement.parentElement.childNodes[3].childNodes[5].childNodes[1].removeEventListener('click',mainSubIconEvent);
+    event.target.removeEventListener('click',addingToCart);
+    setTimeout(()=>{
+        event.target.style.backgroundColor='#038d04';
+        event.target.innerText='Added to Cart';
+    },300);
+    event.target.parentElement.parentElement.childNodes[3].childNodes[1].childNodes[1].addEventListener('click',triggerClick);
+    event.target.parentElement.parentElement.childNodes[3].childNodes[5].childNodes[1].addEventListener('click',triggerClick);
+    event.target.addEventListener('click',shakeCart);
+    if(cartIcon.classList.contains('animateCartIcon')) cartIcon.classList.remove("animateCartIcon");
+    if(notificationMessContainer.classList.contains("slideUp")) {
+        notificationMessContainer.classList.remove("slideUp");
+        notificationMessContainer.classList.add("notifInactive");
+    }
+    var parentCard = event.target.parentElement.parentElement.parentElement;
+    var imageSrc = parentCard.childNodes[1].childNodes[1].src;
+    var medName = parentCard.childNodes[3].innerText;
+    var medLink = parentCard.childNodes[5].childNodes[7].childNodes[1].childNodes[0].href;
+    var medPrice = parentCard.childNodes[5].childNodes[1].childNodes[1].innerText;
+    var company = parentCard.childNodes[5].childNodes[7].childNodes[1].childNodes[0].innerText;
+    var qtyCount = parentCard.childNodes[5].childNodes[3].childNodes[3].innerText;
+    var newMiniCard = `
+        <div class="cartCard">
+            <img src="${imageSrc}"  alt="Medicine">
+            <div class="medDescription">
+                <p class="med_name"><a href="${medLink}" target="_blank">${medName}</a></p>
+                <p class="priceViewParent">
+                    <span class="priceView">${medPrice}</span>
+                </p>
+            </div>
+            <div class="counter">
+                <div><i class="fa-solid fa-minus"></i></div>
+                <p class="val">${qtyCount}</p>
+                <div><i class="fa-solid fa-plus"></i></div>
+            </div>
+            <p class="company_tag">${company}</p>
+            <p class="removeTag">
+                <i class="fa-sharp fa-solid fa-trash"></i>
+            </p>
+        </div> `;
+    cart_view.innerHTML+=newMiniCard;
+    if(summaryBtn.classList.contains('summaryBtnInactive')) summaryBtn.classList.remove('summaryBtnInactive');
+    cartIcon.classList.add("animateCartIcon");
+    if(notificationMessContainer.classList.contains("notifInactive")) {
+        notificationMessContainer.classList.remove("notifInactive");
+        if(count > 1) qtyInCart.innerText = `${count} Items`;
+        else qtyInCart.innerText = `${count} Item`;
+        medNameInCart.innerText = medName;
+        notificationMessContainer.classList.add("slideUp");
+    }   
+}
+
+//function to update the result box based on cart window changes
+function updateResultBoxWhenSummaryClosed(){
+    let cartItems = document.querySelectorAll(".cartCard");
+    let resultCards = document.querySelectorAll(".searchCard");
+    let medLink, flag;
+    for(i=0; i<resultCards.length; i++){
+        flag=0;
+        if(resultCards[i].outerHTML.includes('Added to Cart')||resultCards[i].outerHTML.includes('Already Added!')){
+            medLink=resultCards[i].childNodes[5].childNodes[7].childNodes[1].childNodes[0].href;
+            for(j=0; j<cartItems.length; j++){
+                if(cartItems[j].outerHTML.includes(medLink)){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0){
+                resultCards[i].childNodes[5].childNodes[5].childNodes[1].style='';
+                resultCards[i].childNodes[5].childNodes[5].childNodes[1].innerText='Add to Cart';
+                resultCards[i].childNodes[5].childNodes[3].childNodes[3].innerText=1;
+                resultCards[i].childNodes[5].childNodes[5].childNodes[1].removeEventListener('click',shakeCart);
+                resultCards[i].childNodes[5].childNodes[5].childNodes[1].addEventListener('click',addingToCart);
+                resultCards[i].childNodes[5].childNodes[3].childNodes[1].childNodes[1].removeEventListener('click',triggerClick);
+                resultCards[i].childNodes[5].childNodes[3].childNodes[5].childNodes[1].removeEventListener('click',triggerClick);
+                resultCards[i].childNodes[5].childNodes[3].childNodes[1].childNodes[1].addEventListener('click',mainAddIconEvent);
+                resultCards[i].childNodes[5].childNodes[3].childNodes[5].childNodes[1].addEventListener('click',mainSubIconEvent);
+            }
+        }
+    }
+}
+
+//function to update result box based on cart history
+function updateResultBoxOnSearching(){
+    let cartItems = document.querySelectorAll(".cartCard");
+    let resultCards = document.querySelectorAll(".searchCard");
+    let medLink;
+    for(i=0; i<cartItems.length; i++){
+        medLink= cartItems[i].childNodes[3].childNodes[1].childNodes[0].href;
+        for(j=0; j<resultCards.length; j++){
+            if(resultCards[j].outerHTML.includes(medLink)){
+                resultCards[j].childNodes[5].childNodes[5].childNodes[1].style.backgroundColor='#038d04';
+                resultCards[j].childNodes[5].childNodes[5].childNodes[1].innerText='Added to Cart';
+                resultCards[j].childNodes[5].childNodes[3].childNodes[3].innerText=cartItems[i].childNodes[5].childNodes[3].innerText;
+                resultCards[j].childNodes[5].childNodes[5].childNodes[1].removeEventListener('click',addingToCart);
+                resultCards[j].childNodes[5].childNodes[5].childNodes[1].addEventListener('click',shakeCart);
+                resultCards[j].childNodes[5].childNodes[3].childNodes[1].childNodes[1].removeEventListener('click',mainAddIconEvent);
+                resultCards[j].childNodes[5].childNodes[3].childNodes[5].childNodes[1].removeEventListener('click',mainSubIconEvent);
+                resultCards[j].childNodes[5].childNodes[3].childNodes[1].childNodes[1].addEventListener('click',triggerClick);
+                resultCards[j].childNodes[5].childNodes[3].childNodes[5].childNodes[1].addEventListener('click',triggerClick);
+                break;
+            }
+        }
+    }
+}
+
 //adding event listener to search btn to get to know when exactly to update data
 searchBtn.addEventListener('click', (event)=>{
     event.preventDefault();
@@ -38,94 +180,24 @@ searchBtn.addEventListener('click', (event)=>{
     .then(response => response.json())
     .then(data => {
         displayCards([data.tata1mg, data.apollo, data.pharmeasy]);
-        let addToCartBtns =  document.querySelectorAll(".addToCartBtn");
-        let addIcons = document.querySelectorAll(".addI");
-        let subtractIcons = document.querySelectorAll(".subtractI");
-        let cart_view = document.querySelector(".cart_view");
-        let cartIcon = document.querySelector(".fa-cart-shopping"); 
-        let qtyInCart = document.querySelector(".qtyInCart");
-        let medNameInCart = document.querySelector(".medNameInCart");
+        addToCartBtns =  document.querySelectorAll(".addToCartBtn");
+        addIcons = document.querySelectorAll(".addI");
+        subtractIcons = document.querySelectorAll(".subtractI");
+        cart_view = document.querySelector(".cart_view");
+        cartIcon = document.querySelector(".fa-cart-shopping"); 
+        qtyInCart = document.querySelector(".qtyInCart");
+        medNameInCart = document.querySelector(".medNameInCart");
         //adding eventlisteners to all the required elements of each card
         addIcons.forEach(addIcon=>{
-            addIcon.addEventListener('click', (event)=>{
-                var displayElem = event.target.parentElement.parentElement.childNodes[3];
-                var currentVal = parseInt(displayElem.innerText);
-                displayElem.innerText=currentVal+1;
-            });
+            addIcon.addEventListener('click',mainAddIconEvent);
         });
         subtractIcons.forEach(subtractIcon=>{
-            subtractIcon.addEventListener('click', (event)=>{
-                var displayElem = event.target.parentElement.parentElement.childNodes[3];
-                var currentVal = parseInt(displayElem.innerText);
-                if(currentVal>=2){
-                    displayElem.innerText=currentVal-1;
-                }
-            });
+            subtractIcon.addEventListener('click',mainSubIconEvent);
         });
-        function addingToCart(event){
-            count = parseInt(count) + 1;
-            console.log(count);
-            event.target.removeEventListener('click',addingToCart);
-            setTimeout(()=>{
-                event.target.style.backgroundColor='#038d04';
-                event.target.innerText='Added to Cart';
-            },300);
-            event.target.addEventListener('click',()=>{
-                event.target.style.animation='shake 0.5s';
-                event.target.style.backgroundColor='#be0404';
-                event.target.innerText='Already Added!'
-                setTimeout(()=>{
-                    event.target.style.animation='';
-                },500);
-            });
-            if(cartIcon.classList.contains('animateCartIcon')) cartIcon.classList.remove("animateCartIcon");
-            if(notificationMessContainer.classList.contains("slideUp")) {
-                notificationMessContainer.classList.remove("slideUp");
-                notificationMessContainer.classList.add("notifInactive");
-            }
-            var parentCard = event.target.parentElement.parentElement.parentElement;
-            var imageSrc = parentCard.childNodes[1].childNodes[1].src;
-            var medName = parentCard.childNodes[3].innerText;
-            var medLink = parentCard.childNodes[5].childNodes[7].childNodes[1].childNodes[0].href;
-            var medPrice = parentCard.childNodes[5].childNodes[1].childNodes[1].innerText;
-            var company = parentCard.childNodes[5].childNodes[7].childNodes[1].childNodes[0].innerText;
-            // var count = parentCard.childNodes[5].childNodes[3].childNodes[3].innerText;
-            var newMiniCard = `
-                <div class="cartCard">
-                    <img src="${imageSrc}"  alt="Medicine">
-                    <div class="medDescription">
-                        <p class="med_name"><a href="${medLink}" target="_blank">${medName}</a></p>
-                        <p class="priceViewParent">
-                            <span class="priceView">${medPrice}</span>
-                        </p>
-                    </div>
-                    <div class="counter">
-                        <div><i class="fa-solid fa-minus"></i></div>
-                        <p class="val">1</p>
-                        <div><i class="fa-solid fa-plus"></i></div>
-                    </div>
-                    <p class="company_tag">${company}</p>
-                    <p class="removeTag">
-                        <i class="fa-sharp fa-solid fa-trash"></i>
-                    </p>
-                </div> `;
-            cart_view.innerHTML+=newMiniCard;
-            if(summaryBtn.classList.contains('summaryBtnInactive')) summaryBtn.classList.remove('summaryBtnInactive');
-            cartIcon.classList.add("animateCartIcon");
-            if(notificationMessContainer.classList.contains("notifInactive")) {
-                notificationMessContainer.classList.remove("notifInactive");
-                if(count > 1) qtyInCart.innerText = `${count} Items`;
-                else qtyInCart.innerText = `${count} Item`;
-                medNameInCart.innerText = medName;
-                notificationMessContainer.classList.add("slideUp");
-            }
-            
-        }
         addToCartBtns.forEach(btn=>{
-            
             btn.addEventListener('click',addingToCart);
-            
         });
+        updateResultBoxOnSearching();
         //removing previous event listener and adding new one
         summaryBtn.removeEventListener('click', summaryHandling);
         //Implemenation of event listeners on Cart Container Items
@@ -152,6 +224,7 @@ searchBtn.addEventListener('click', (event)=>{
             updateSummary();
         }
         function delAction(event){
+            count--;
             var cartCards = document.querySelectorAll(".cartCard");
             miniCards=[];
             cartCards.forEach(card=>{
@@ -233,7 +306,7 @@ function displayCards(medJsonData){
                             <div class="price">
                                 Price- <span>${(medJsonData[i].prices[j]).replace('MRP','')}</span>
                             </div>
-                            <!--<div class="quantityArea">
+                            <div class="quantityArea">
                                 <div class="addIcon">
                                     <span class="material-symbols-outlined addI" style="width: 63px;">
                                         add
@@ -247,7 +320,7 @@ function displayCards(medJsonData){
                                         remove
                                     </span>
                                 </div>
-                            </div>-->
+                            </div>
                             <div class="addToCartArea">
                                 <div class="addToCartBtn">
                                     Add to Cart
@@ -290,6 +363,7 @@ viewCartBtn.addEventListener('click', () => {
 closeIcon.addEventListener('click', () => {
     resultBox.style.height='';
     summaryBox.style.top='40%';
+    updateResultBoxWhenSummaryClosed();
     summaryContainer.classList.remove("active");
     searchResultBox.classList.remove("inactive");
     viewCartBtn.classList.remove("inactive");
