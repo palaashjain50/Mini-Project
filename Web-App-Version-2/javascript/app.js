9ea6f10c346687d7ee0fe4e7f47bff2ba8ccdad4
HarshJ2508 <harsh.jain2@spit.ac.in>
2024-03-08 21:44:27 +0530

    cors issue resolved

--- Web-App-Version-2/javascript/app.js
+++ Web-App-Version-2/javascript/app.js
@@ -79,12 +79,15 @@ const app = express();
 
 // app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
-app.use(function(req, res, next){
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+// app.use(function(req, res, next){
+//     res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+// });
+
+const cors = require('cors')
+app.use(cors())
 
 app.post('/', async (req, res) => {
       
